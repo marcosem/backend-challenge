ec98cb45ec4a8069556df1280fef960d47f0544a
Marcos Mathias <marcos-github@memathias.com>
2021-07-31 18:33:41 -0300

    feat: Created app.ts

---
+++ packages/challengesApi/src/shared/container/index.ts
@@ -0,0 +1,19 @@
+import { container } from 'tsyringe';
+
+import IChallengesRepository from '@modules/challenges/repositories/IChallengesRepository';
+import ChallengesRepository from '@modules/challenges/infra/typeorm/repositories/ChallengesRespository';
+
+import ISubmissionsRepository from '@modules/submissions/repositories/ISubmissionsRepository';
+import SubmissionsRepository from '@modules/submissions/infra/typeorm/repositories/SubmissionsRepository';
+
+container.registerSingleton<IChallengesRepository>(
+  'ChallengesRepository',
+  ChallengesRepository,
+);
+
+container.registerSingleton<ISubmissionsRepository>(
+  'SubmissionsRepository',
+  SubmissionsRepository,
+);
+
+// for another repository, just duplicate
---
+++ packages/challengesApi/src/shared/infra/http/app.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+// import { errors } from 'celebrate';
+import 'express-async-errors';
+import '@shared/infra/typeorm';
+import '@shared/container';
+import AppError from '@shared/errors/AppError';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// app.use(errors());
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+export default app;
