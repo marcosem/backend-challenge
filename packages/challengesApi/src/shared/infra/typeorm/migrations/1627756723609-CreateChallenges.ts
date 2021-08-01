33b57d6d1691f0160ab27f06525d9f7100382898
Marcos Mathias <marcos-github@memathias.com>
2021-08-01 18:31:28 -0300

    fix: Missing uuid function on migration

--- packages/challengesApi/src/shared/infra/typeorm/migrations/1627756723609-CreateChallenges.ts
+++ packages/challengesApi/src/shared/infra/typeorm/migrations/1627756723609-CreateChallenges.ts
@@ -4,6 +4,8 @@ export default class CreateChallenges1627756723609
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     await queryRunner.createTable(
       new Table({
         name: 'challenges',
